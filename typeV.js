9d208111ed5b376a778fe7aeff8337f504c0b266
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2019-08-31 23:02:32 +1000

    with typescript

---
+++ typeV.js
@@ -0,0 +1,14 @@
+// get that all
+// all supported variable
+var isDone = false;
+var strings = '';
+var numbers = 1;
+// arrays
+var numberArrays = [1, 23, 4, 5, 12, 3];
+var numberArray1 = [2, 3, 4, 1, 23];
+var stringArrays = ['name', 'jokes', 'happens'];
+var stringArray2 = ['name', 'firstname', 'lastname'];
+// basic variables
+stringArray2 = ['you have ', 'the fist name', 'web developer'];
+console.log(stringArray2);
+// enum
--- typeV.ts
+++ typeV.ts
@@ -1,1 +1,21 @@
 // get that all
+// all supported variable
+let isDone: boolean = false;
+let strings :string = '';
+let numbers :number =1;
+// arrays
+let numberArrays:number[]= [1,23,4,5,12,3];
+
+let numberArray1:Array<number> = [2,3,4,1,23]
+
+let stringArrays:string[]=['name','jokes','happens'];
+
+let stringArray2:Array<string> =['name','firstname','lastname'];
+// basic variables
+
+stringArray2=['you have ','the fist name','web developer'];
+
+console.log(stringArray2);
+
+// enum
+
