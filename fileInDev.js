083c201356a4969793d15858c6c6ef2554379fc1
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2019-08-31 22:28:24 +1000

    update messages

--- fileInDev.js
+++ fileInDev.js
@@ -1,1 +1,20 @@
-console.log('this is a file exist in the dev file')
+
+let totalArray = [];
+
+let array1 =[2,1,4,5];
+let array2 = [3,4,9,5,6,5,3]
+
+totalArray =[array1,array2]
+
+
+const saveArray =(x,y,number,baseArray)=>{
+    console.log(baseArray);
+    if(baseArray[x][y] !==null){
+        baseArray[x][y] =number;
+    }
+    return baseArray;
+
+}
+finalArray = saveArray(0,6,5,totalArray)
+// console.log(...finalArray)
+// console.log(totalArray[0][1])
