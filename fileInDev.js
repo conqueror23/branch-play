cc225d46be30e0d85d3bd959ef1092409f9a0a0c
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2019-08-31 22:42:18 +1000

    kysotu

--- fileInDev.js
+++ fileInDev.js
@@ -1,20 +1,17 @@
-
-let totalArray = [];
-
-let array1 =[2,1,4,5];
-let array2 = [3,4,9,5,6,5,3]
-
-totalArray =[array1,array2]
-
-
-const saveArray =(x,y,number,baseArray)=>{
-    console.log(baseArray);
-    if(baseArray[x][y] !==null){
-        baseArray[x][y] =number;
+const isValid= (x,y,number)=>{
+    let totalArray = [];
+    let array1 =[2,1,4,5];
+    let array2 = [3,4,9,5,6,5,3]
+    totalArray =[array1,array2]
+    const saveArray =(x,y,number,baseArray)=>{
+        if(baseArray[x][y] !==null){
+            baseArray[x][y] =number;
+        }
+        return baseArray;
     }
-    return baseArray;
-
+    finalArray = saveArray(0,6,5,totalArray)
+    // console.log(finalArray)
+    return true
 }
-finalArray = saveArray(0,6,5,totalArray)
-// console.log(...finalArray)
-// console.log(totalArray[0][1])
+let result= isValid(0,2,4)
+console.log(result)
