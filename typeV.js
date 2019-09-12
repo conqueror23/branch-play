df98707f234f63f23a856e1224adc281c5ed9b78
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2019-09-12 19:23:48 +1000

    uipdat

--- typeV.js
+++ typeV.js
@@ -11,4 +11,54 @@ var stringArray2 = ['name', 'firstname', 'lastname'];
 // basic variables
 stringArray2 = ['you have ', 'the fist name', 'web developer'];
 console.log(stringArray2);
+// tuple 
+var tuple;
+tuple = ['name', 23];
+console.log(tuple);
 // enum
+var color;
+(function (color) {
+    color[color["red"] = 0] = "red";
+    color[color["green"] = 1] = "green";
+    color[color["blue"] = 2] = "blue";
+})(color || (color = {}));
+var c = color.green;
+// enums2
+var color2;
+(function (color2) {
+    color2[color2["red"] = 1] = "red";
+    color2[color2["green"] = 2] = "green";
+    color2[color2["brown"] = 3] = "brown";
+})(color2 || (color2 = {}));
+var colorName = color[2];
+console.log(colorName);
+// any type
+var noSure;
+noSure = 'just a test';
+noSure = '21';
+console.log(noSure);
+// void 
+// as the opsite of any , there would be nothing but returns???
+function voidReturns() {
+    console.log('this is indies a void function ');
+}
+var unusable = undefined;
+unusable = null;
+// undefined and null
+var udfe = undefined;
+var nul = null;
+// never ??
+function error(message) {
+    throw new Error(message);
+}
+function fail() {
+    return error('somthing failed');
+}
+function infinitLoop() {
+    while (true) {
+        console.log('show the error message');
+    }
+}
+var object1 = create({ prop: 1 });
+var object2 = create({ 'name': 'this' });
+console.log("object1 :" + object1 + ", object2 :" + object2);
--- typeV.ts
+++ typeV.ts
@@ -16,6 +16,78 @@ let stringArray2:Array<string> =['name','firstname','lastname'];
 stringArray2=['you have ','the fist name','web developer'];
 
 console.log(stringArray2);
+// tuple 
+let tuple: [string,number]
+
+tuple = ['name',23];
+
+console.log(tuple)
 
 // enum
+
+enum color {red,green,blue}
+
+let c:color = color.green;
+
+// enums2
+
+enum color2{red=1,green,brown}
+
+let colorName:string=color[2];
+
+console.log(colorName);
+
+// any type
+
+let noSure :any ;
+
+noSure = 'just a test'
+
+noSure = '21'
+console.log(noSure)
+
+// void 
+// as the opsite of any , there would be nothing but returns???
+function voidReturns():void {
+    console.log('this is indies a void function ')
+}
+
+
+let unusable :void = undefined 
+
+unusable = null;
+
+// undefined and null
+
+let udfe:undefined =undefined;
+
+let nul:null =null ;
+
+// never ??
+
+function error(message:string ) :never{
+    throw new Error(message);
+}
+function fail(){
+    return error('somthing failed')
+}
+function infinitLoop():never{
+    while (true){
+        console.log('show the error message')
+    }
+}
+
+// object
+
+declare function create(obj:object |null):void;
+
+let object1 =create({prop:1})
+
+let object2 =create({'name':'this'})
+
+console.log(`object1 :${object1}, object2 :${object2}`);
+
+
+
+
 
