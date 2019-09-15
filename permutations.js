ead89e37b0069f6ad3999324656c67724fd12b7f
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2019-09-15 23:44:08 +1000

    add permuations

--- permutations.js
+++ permutations.js
@@ -1,45 +1,106 @@
-const getPermutations=(array)=>{
-    let perms = [];
-    array.map((res,index,array)=>{
-        let current =array.pop();
-        
-        perms.push(current);
-        
-        // select a start point 
-        // push start point to array 
-        // get a second start point 
-        // 
+const getPermutations = array => {
+  let perms = [];
+  array.map((res, index, array) => {
+    let current = array.pop();
 
-        // console.log(array)
-    })
-}
+    perms.push(current);
 
-let array1 =[1,2,3,4]
+    // select a start point
+    // push start point to array
+    // get a second start point
+    //
+
+    // console.log(array)
+  });
+};
+
+let array1 = [1, 2, 3, 4];
 function perm(xs) {
-    let ret = [];
-    for (let i = 0; i < xs.length; i = i + 1) {
-      let rest = perm(xs.slice(0, i).concat(xs.slice(i + 1)));
+  let ret = [];
+  for (let i = 0; i < xs.length; i = i + 1) {
+    let rest = perm(xs.slice(0, i).concat(xs.slice(i + 1)));
     //   console.log(rest)
-      if(!rest.length) {
-        ret.push([xs[i]])
-      } else {
-        for(let j = 0; j < rest.length; j = j + 1) {
-          ret.push([xs[i]].concat(rest[j]))
+    if (!rest.length) {
+      ret.push([xs[i]]);
+    } else {
+      for (let j = 0; j < rest.length; j = j + 1) {
+        ret.push([xs[i]].concat(rest[j]));
         //   console.log(ret)
-        }
       }
     }
-    return ret;
   }
-  
+  return ret;
+}
+
 //   slice and concat
 
-let slice1 = array1.slice(1)
-let slice = array1.slice(0,0)
-let rest =slice.concat(slice1)
-console.log(rest)
+// better solutions
+const permutebetter = nums => {
+  var numsList = [];
+  var permList = [];
+  permuteHelper(nums, numsList, permList);
+  return permList;
+};
 
-// let concat1 = slice1.concat(4,4)
-// console.log(concat1)
-let permsn = perm(array1)
-// console.log(permsn)
+var permuteHelper = function(nums, numsList, permList) {
+  if (nums == undefined || nums.length == 0) {
+    permList.push(numsList);
+    return;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    let newNumsList = numsList.slice(0);
+    newNumsList.push(nums[i]);
+
+    let tempNums = nums.slice(0);
+    tempNums.splice(i, 1);
+    permuteHelper(tempNums, newNumsList, permList);
+  }
+
+  return;
+};
+
+// 2nd best solutions
+const permute2nd = nums => {
+  const ans = [];
+  const swap = (i, j) => {
+    const tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  };
+  const recur = first => {
+    if (first === nums.length) {
+      ans.push([...nums]);
+    } else {
+      for (let i = first; i < nums.length; i++) {
+        swap(first, i);
+        recur(first + 1);
+        swap(first, i);
+      }
+    }
+  };
+  recur(0);
+  return ans;
+};
+// permute Quickest
+const permuteQ = nums => {
+  let res = [];
+  let visited = new Array(nums.length).fill(0);
+  helper(nums, 0, visited, [], res);
+  return res;
+  function helper(nums, level, visited, temp, res) {
+    if (level === nums.length) {
+      res.push(temp.slice());
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (visited[i] === 1) continue;
+      visited[i] = 1;
+      temp.push(nums[i]);
+      //console.log(temp)
+      helper(nums, level + 1, visited, temp, res);
+      temp.pop();
+      visited[i] = 0;
+    }
+  }
+};
