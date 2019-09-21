465407748516f4270a79d25f0907849bc9bb310c
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2019-09-21 22:54:43 +1000

    add node server

--- server1.js
+++ server1.js
@@ -1,12 +1,27 @@
 var express = require('express')
-// var bodyParse = require('body-parser')
+var bodyParser = require('body-parser')
 
 const app= express();
 
-// app.use(bodyParse)
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/',(req,res)=>{
-    res.send('helloe')
+    res.send('helloe just ing sfherer and you got all these all the way')
 })
+
+app.post('/items',(req,res)=>{
+    let size= draw(req.body.x,req.body.y);
+    // res.writeHead(400,'text/html')
+    res.send(`this is what your name updated ${req.body.name}, and this is the email you input ${req.body.email}, the size of this ${size} `)
+})
+
+const draw =(dx,dy)=>{
+    let size=  dx*dy;
+    return size
+}
+
 
 app.listen(3000)
